a2252730fd4a5b0973526f73ccf13702b89363f5
GlamnikiA <ardian.glamniki@hotmail.com>
2021-03-22 11:22:46 +0100

    Update MathProblems.java
    Test

--- src/model/MathProblems.java
+++ src/model/MathProblems.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class MathProblems {
     public static ArrayList<String> getMathProblems(String file) throws FileNotFoundException {
         Scanner in = new Scanner(new File(file));
-
+//Test
         ArrayList<String> mathProblems = new ArrayList<>();
         while(in.hasNextLine()) {
             mathProblems.add(in.nextLine());

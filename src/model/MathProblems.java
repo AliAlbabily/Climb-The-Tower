01f1f54b1b9a8418fe34e9489ffe93de5ae7c9c0
GlamnikiA <ardian.glamniki@hotmail.com>
2021-03-22 11:08:09 +0100

    Update MathProblems.java
    Added comments

--- src/model/MathProblems.java
+++ src/model/MathProblems.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MathProblems {
+    /*
+    * This method fetches math problems from a file and puts them in a list
+    * Returntype: Arraylist
+    */
     public static ArrayList<String> getMathProblems(String file) throws FileNotFoundException {
         Scanner in = new Scanner(new File(file));
 

7b5a54adbfcaf5d5d24af4f6da1f888b3fc53b33
hanisss <81147824+hanisss@users.noreply.github.com>
2021-04-04 16:21:39 +0200

    Levels forts
    Level 6 vet ej om vi ska fixa så att man skriver in double eller läser frågor från fil?

--- src/model/levels/Level6.java
+++ src/model/levels/Level6.java
@@ -3,12 +3,21 @@ package model.levels;
 import model.Level;
 import model.Monster;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * @author Hanis Saley
  * version 1.0
  */
 public class Level6 extends Level {
 
+    private HashMap<String, Integer> map = new HashMap<>();
+    private ArrayList<String> mathProblemsLevel6;
 
     public Level6(String lvlName) {
         super(lvlName);
@@ -22,9 +31,23 @@ public class Level6 extends Level {
         b = (int)(Math.random() * 10 +1);
         c = (int)(Math.random() * 10 +1);
 
-        System.out.println(a + " * " + b + "/" + c);
-        System.out.println(a*b/c);
-        System.out.println("round down the answer for the time being");
-        return a*b/c; //läsa från fil eller fixa så att man ska skriva in double?
+        if(a * b > c){ // fixa så att man skriver in double?? eller läsa från fil?
+            System.out.println(a + " * " + b + "/" + c);
+            System.out.println(a*b/c);
+            System.out.println("round down the answer första if satsen");
+            return a*b/c;
+        }
+        else if(a * b < c){
+            System.out.println(c + " * " + b + "/" +a);
+            System.out.println(c*b/a);
+            System.out.println("round down the answer if else satsen");
+            return c*b/a;
+        }
+        else{
+            System.out.println(c + " * " + a + "/" +b);
+            System.out.println(c*a/b);
+            System.out.println("round down the answer else satsen");
+            return c*a/b;
+        }
     }
 }

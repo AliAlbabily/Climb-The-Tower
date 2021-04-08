6b0d031e463ae2cef9805c15a734fce35b499f66
DeGGi <81147016+DeGGiii@users.noreply.github.com>
2021-04-08 16:48:42 +0200

    Lagt till mina GUI filer
    Lagt till mina klasser nu efter haft fel placerat de.

--- src/view/MainFrameGUI.java
+++ src/view/MainFrameGUI.java
@@ -1,4 +1,77 @@
 package view;
 
-public class MainFrameGUI {
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrameGUI
+{
+    private JFrame frame;
+    private JPanel pnlNorth;
+    private JPanel pnlSouth;
+
+
+
+    public MainFrameGUI()
+    {
+        InitializePanels();
+    }
+
+    public void InitializePanels()
+    {
+        createMainFrame();
+        NorthPanel();
+        SouthPanel();
+    }
+
+    public void createMainFrame()
+    {
+        frame = new JFrame("Climb The Tower");
+        frame.setPreferredSize(new Dimension(1000, 600));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.pack();
+    }
+
+    public void NorthPanel()
+    {
+        pnlNorth = new JPanel();
+        pnlNorth.setPreferredSize(new Dimension(1000,400));
+        pnlNorth.setBackground(Color.gray);
+        pnlNorth.setVisible(true);
+        pnlNorth.setLayout(null);
+
+        JLabel lblQuestion = new JLabel("35 + 11 = ?");
+        lblQuestion.setForeground(Color.white);
+        lblQuestion.setFont(new Font(Font.SANS_SERIF, Font.PLAIN,30));
+        lblQuestion.setBounds(100, 125, 200, 50);
+
+        JTextField answerTextField = new JTextField();
+        answerTextField.setBounds(75,175,200, 25);
+
+        pnlNorth.add(lblQuestion);
+        pnlNorth.add(answerTextField);
+
+        frame.add(pnlNorth, BorderLayout.NORTH);
+        frame.pack();
+    }
+
+    public void SouthPanel()
+    {
+        pnlNorth = new JPanel();
+        pnlNorth.setPreferredSize(new Dimension(1000,200));
+        pnlNorth.setBackground(Color.BLACK);
+        pnlNorth.setVisible(true);
+        pnlNorth.setLayout(null);
+
+        frame.add(pnlSouth, BorderLayout.SOUTH);
+        frame.pack();
+    }
+
+    public static void main(String[] args)
+    {
+        MainFrameGUI mainFrameGUI = new MainFrameGUI();
+    }
 }
+
---
+++ src/view/StartMenuGUI.java
@@ -0,0 +1,104 @@
+package view;
+
+import control.Controller;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StartMenuGUI extends JPanel implements ActionListener
+{
+    //en instans av controller
+    private Controller controller;
+
+    //instansiering av java swing variabler
+    private JTextField userTextField = new JTextField();
+    private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
+    private JButton playBtn = new JButton();
+    private JButton infoBtn = new JButton();
+
+   // private Image pic;
+//    private Image img = Toolkit.getDefaultToolkit().getImage("files/towers.png");
+
+    public StartMenuGUI()
+    {
+        //this.controller = controller;
+        createMenuBox();
+        //ImageIcon obj = new ImageIcon("files/towers.png");
+        //pic = obj.getImage();
+
+    }
+//    //TODO fixa backgrund bild
+//    @Override
+//    protected void paintComponent(Graphics g)
+//    {
+//
+//        super.paintComponent(g);
+//        g.drawImage(pic, 0, 0, null);
+//    }
+
+
+    public void createMenuBox()
+    {
+        frame = new JFrame("Climb The Tower");
+        frame.setSize(300, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        Border blackline = BorderFactory.createLineBorder(Color.black);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setPreferredSize(new Dimension(300, 350));
+        mainPanel.setLayout(null);
+
+
+        panel.add(mainPanel);
+
+        userTextField = new JTextField("");
+        userTextField.setPreferredSize(new Dimension(200,30));
+        userTextField.setBounds(50,175, 200, 30);
+        userTextField.setBorder(blackline);
+
+        playBtn = new JButton("PLAY");
+        playBtn.setBounds(75, 225, 150, 50);
+        infoBtn = new JButton("?");
+        infoBtn.setBounds(15, 2, 40,40);
+        infoBtn.setFont(new Font("", Font.BOLD, 10));
+        infoBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane optionPane = new JOptionPane
+                        ("You versus the monsters! You play the game by typing the the right mathematical answers for each question. \n"+
+                                "An amount of correct answers are required to defeat the monsters. \n"+
+                                "The further you progress the harder the questions become. \n" +
+                                "You can also get bonus hits and points if you answer quickly before the timer" +
+                                " runs out.\n" + "Good luck warrior!",JOptionPane.OK_OPTION);
+                JDialog dialog = optionPane.createDialog("Dialog");
+                dialog.setVisible(true);
+            }
+        });
+
+        mainPanel.add(infoBtn);
+        mainPanel.add(userTextField);
+        mainPanel.add(playBtn);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        StartMenuGUI startMenu = new StartMenuGUI();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
